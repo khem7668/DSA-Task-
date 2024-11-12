760d2775d6e48fdbeaa016ce5c47bafdccdcf94f
khem7668 <122192759+khem7668@users.noreply.github.com>
2024-11-12 09:18:55 +0530

    Create 237DeleteNodeInLinkedList.java

---
+++ 237DeleteNodeInLinkedList.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode current = node.next;
+        node.val = current.val;
+        node.next= current.next;
+
+       
+    }
+}
